c64036dc8159f3a849feaeaaa628f7b93bcddc5d
mrsalesi <mrsalesi@gmail.com>
2019-07-22 21:04:57 +0430

    Update jj2.js
    1940

--- HMIS/web/Manager/js/jj2.js
+++ HMIS/web/Manager/js/jj2.js
@@ -374,50 +374,50 @@ var jj = function (selector) {
         var com_textarea = $(this.selector + " textarea");
         for (var i = 0; i < com_hidden.length; i++) {
             if (com_hidden[i].name != "") {
-                param += (param == "" ? "" : "&") + com_hidden[i].name + "=" + new jj("#" + com_hidden[i].id).jjVal();
+                param += (param == "" ? "" : "&") + com_hidden[i].name + "=" + new jj(this.selector + " #" + com_hidden[i].id).jjVal();
             }
         }
         for (var i = 0; i < com_text.length; i++) {
-            if (com_text[i].name != "") {
-                param += (param == "" ? "" : "&") + com_text[i].name + "=" + new jj("#" + com_text[i].id).jjVal();
+            if (com_text[i].name != "" && com_text[i].id != "") {
+                param += (param == "" ? "" : "&") + com_text[i].name + "=" + new jj(this.selector + " #" + com_text[i].id).jjVal();
             }
         }
         for (var i = 0; i < com_checkbox.length; i++) {
-            if (com_checkbox[i].name != "") {
-                param += (param == "" ? "" : "&") + com_checkbox[i].name + "=" + new jj("#" + com_checkbox[i].id).jjVal();
+            if (com_checkbox[i].name != "" && com_checkbox[i].id != "") {
+                param += (param == "" ? "" : "&") + com_checkbox[i].name + "=" + new jj(this.selector + " #" + com_checkbox[i].id).jjVal();
             }
         }
         for (var i = 0; i < com_number.length; i++) {
-            if (com_number[i].name != "") {
-                param += (param == "" ? "" : "&") + com_number[i].name + "=" + new jj("#" + com_number[i].id).jjVal();
+            if (com_number[i].name != "" && com_number[i].id != "") {
+                param += (param == "" ? "" : "&") + com_number[i].name + "=" + new jj(this.selector + " #" + com_number[i].id).jjVal();
             }
         }
         for (var i = 0; i < com_radio.length; i++) {
-            if (com_radio[i].name != "") {
+            if (com_radio[i].name != "" && com_radio[i].id != "") {
                 if (new jj("#" + com_radio[i].id).jjVal() == 1) {
-                    param += (param == "" ? "" : "&") + com_radio[i].name + "=" + $("#" + com_radio[i].id).attr("value");
+                    param += (param == "" ? "" : "&") + com_radio[i].name + "=" + $(this.selector + " #" + com_radio[i].id).attr("value");
                 }
             }
         }
         for (var i = 0; i < com_select.length; i++) {
-            if (com_select[i].name != "") {
-                param += (param == "" ? "" : "&") + com_select[i].name + "=" + new jj("#" + com_select[i].id).jjVal();
+            if (com_select[i].name != "" && com_select[i].id != "") {
+                param += (param == "" ? "" : "&") + com_select[i].name + "=" + new jj(this.selector + " #" + com_select[i].id).jjVal();
             }
         }
         for (var i = 0; i < com_password.length; i++) {
-            if (com_password[i].name != "") {
-                param += (param == "" ? "" : "&") + com_password[i].name + "=" + new jj("#" + com_password[i].id).jjVal();
+            if (com_password[i].name != "" && com_password[i].id != "") {
+                param += (param == "" ? "" : "&") + com_password[i].name + "=" + new jj(this.selector + " #" + com_password[i].id).jjVal();
             }
         }
         for (var i = 0; i < com_textarea.length; i++) {
-            if (com_textarea[i].name != "") {
+            if (com_textarea[i].name != "" && com_textarea[i].id != "") {
                 //                alert(com_textarea[i].style.visibility);
                 if ($(com_textarea[i]).hasClass("summernote")) {
                     var val = encodeURIComponent($(("#" + com_textarea[i].id)).summernote('code'));
                     val = (new jj(new jj(val).jjTrim()).jjGetUtf8());
                     param += (param == "" ? "" : "&") + com_textarea[i].name + "=" + val;
                 } else {// اگر تکست اریا معمولی باشد
-                    param += (param == "" ? "" : "&") + com_textarea[i].name + "=" + new jj("#" + com_textarea[i].id).jjVal();
+                    param += (param == "" ? "" : "&") + com_textarea[i].name + "=" + new jj(this.selector + " #" + com_textarea[i].id).jjVal();
                 }
             }
         }
@@ -426,6 +426,7 @@ var jj = function (selector) {
     this.jjFormClean = function () {
         var com_checkbox = $(this.selector + " input:checkbox");
         var com_text = $(this.selector + " input:text");
+        var com_text = $(this.selector + " input:hidden");
         var com_radio = $(this.selector + " input:radio");
         var com_password = $(this.selector + " input:password");
         var com_file = $(this.selector + " input:file");
@@ -1908,13 +1909,15 @@ var jj = function (selector) {
             });
         });
     };
+
+
     /**این تابع برای attach file 
      * اضافه کردن چند فایل نوشته شده,
      *اضافه کردن عنوان به تابع است
      * @param selector is button for send
      * @example jj("#btnSendId").jjAjaxFileUpload('inputFileId','#inputTextId','#viewImgId');
      * <script type="text/javascript" src="js/jquery/ajaxfileupload.js"></script>
-
+     
      * divUploadSelectorبرای قراردادن قایل های اپلود شده در دیو
      */
 
@@ -1944,7 +1947,7 @@ var jj = function (selector) {
                     if (data != null) {
                         data = data.replace('<pre style="word-wrap: break-word; white-space: pre-wrap;">', '');
                         data = data.replace('<PRE style="word-wrap: break-word; white-space: pre-wrap;">', '');
-                        data = data.replace("<PRE>", '').replace("</PRE>",'').replace("<pre>", '').replace("</pre>", '').replace("upload/", '').replace("Upload/", '');
+                        data = data.replace("<PRE>", '').replace("</PRE>", '').replace("<pre>", '').replace("</pre>", '').replace("upload/", '').replace("Upload/", '');
                         data = data.replace("/", '').replace("/", '').replace("\\", '');
                     } else {
                         new jj('فایل به درستی ارسال نشد.').jjModal();
@@ -1964,11 +1967,11 @@ var jj = function (selector) {
                             for (var j = 0; j < temp.size(); j++) {
                                 if (ext == ("png") || ext == ("jpg") || ext == ("gif") || ext == ("svg")) {
 
-                                    $(divUploadSelector).append("<div><img   class='col-xs-12' style='width:10%;float:right' src='upload/" + data + "'/>\n\
-<input  class='col-xs-12 form-control ' disabled='disabled' value='" + $("#" + inputFiletitle.replace("#", "")).val() + "'>"
+                                    $(divUploadSelector).append("<div><img   class='col-xs-12' style='width:10%;float:right' src='upload/" + data + "'/>"
+                                            + "<input  class='col-xs-12 form-control ' disabled='disabled' value='" + $("#" + inputFiletitle.replace("#", "")).val() + "'>"
                                             + " <input class='col-xs-12 form-control file' disabled='disabled' value='" + data + "'>"
                                             + "<button  class='buttonRemove col-lg-1' style='background-color: #e16262;color: white;float:left' onclick=' $(this).parent().remove();' >حذف</button>"
-                                            + "<a  class='col-lg-1' style='background-color: green;color: white;float:left;text-align: center;padding-top: 2px;padding-bottom: 1px;margin-top: 1px;' href='upload/" + data + "' >دانلود</a></div>");   
+                                            + "<a  class='col-lg-1' style='background-color: green;color: white;float:left;text-align: center;padding-top: 2px;padding-bottom: 1px;margin-top: 1px;' href='upload/" + data + "' >دانلود</a></div>");
                                 } else {
                                     $(divUploadSelector).append("<div><input  class='col-xs-12 form-control ' disabled='disabled' value='" + $("#" + inputFiletitle.replace("#", "")).val() + "'>" + " <input class='col-xs-12 form-control file' disabled='disabled' value='" + data + "'>" + "<button  class='buttonRemove col-lg-1' style='background-color: #e16262;color: white;float:left' onclick=' $(this).parent().remove();' >حذف</button>" + "<a  class='col-lg-1' style='background-color: green;color: white;float:left;text-align: center;padding-top: 2px;padding-bottom: 1px;margin-top: 1px;' href='upload/" + data + "' >دانلود</a></div>");
 
@@ -1989,6 +1992,81 @@ var jj = function (selector) {
         });
     };
 
+    /**
+     * برای اینکه در قسمت هایی که کاربر محدودیت بارگذاری فایل ندارد و می تواند چندین فایل با عنوان دلخواه وارد کند
+     * دقت شود اسم ستون دیتا بیس باید پارامتر دوم داده شود و سریال کردن بعد از آن با خودمان است
+     * @param {type} inputFileId فایل بروزر  را باید اینجا وارد کنیم
+     * @param {type} databaseColoumnName توصیه میشود در کل پروزه  یونیک باشد که اشتبا از فرم های دیگر سریال نشود یا خودتان حواستان باشد
+     *  در نهایت نام  فایل های بارگذاری شده در یک اینپوت فیلد مربوط به همان عکس قرار میگیرد که با این کلاس مشخص و بعد در هنگام نیاز بر اساس این کلاس سریال می شود
+     * @param {type} inputTilteSelector کاربر عنوان فایلش را در اینجا وارد می کند و بعد از بارگذاری فایل نامش را به سرور برای آپدیت می فرستیم و همین را برای نشان دادن به کاربر می گذاریم
+     * @param {type} divUploadSelector این دیوی است که باید  فایل های آپلود شده در آن نمایش داده بشوند
+     * @returns {undefined}
+     */
+    this.jjAjaxFileUploadByTitleAndMultiFile = function (inputFileId, databaseColoumnName, inputFiletitle, divUploadSelector) {
+        $(this.selector).click(function () {
+            if ($("#" + inputFileId.replace("#", "")).val() == "") {
+                new jj("ابتدا  فایلی را انتخاب نمایید.").jjModal();
+                return;
+            }
+            $.ajaxFileUpload({
+                url: 'UploadServlet',
+                secureuri: false,
+                fileElementId: inputFileId.replace("#", ""),
+                fileElementTitle: inputFiletitle.replace("#", ""),
+                dataType: 'JSON',
+                cache: false,
+                success: function (data) {
+                    var title = $("#" + inputFiletitle.replace("#", "")).val()
+                    var param = "";
+                    param += "&title=" + title;
+                    param += "&filename=" + data;
+                    param += "&do=Upload.setTitle";
+                    new jj(param).jjAjax2(false);
+                    if (data != null) {
+                        data = data.replace('<pre style="word-wrap: break-word; white-space: pre-wrap;">', '');
+                        data = data.replace('<PRE style="word-wrap: break-word; white-space: pre-wrap;">', '');
+                        data = data.replace("<PRE>", '').replace("</PRE>", '').replace("<pre>", '').replace("</pre>", '').replace("upload/", '').replace("Upload/", '');
+                        data = data.replace("/", '').replace("/", '').replace("\\", '');
+                    } else {
+                        new jj('فایل به درستی ارسال نشد.').jjModal();
+                    }
+                    $("#" + inputFileId.replace("#", "")).val('');
+
+                    if (data != "") {
+                        $("#" + inputFileId.replace("#", "")).val('');
+                        if (data != "big") {
+                            var picName = data;
+                            var ext = data.split('.').pop();
+                            // برای آلنکه بتوانیم چندین اینپوت فیلد که مربوط به یک ستون در دیتابیس است را سریال کنیم نام کلاس را دقیقا ستون معادل در دیتابیس می گذاریم و باید در هنگام ادیت یا اینسرت با یک حلقه ی تکرار این سریال را انجام دهیم
+                            if (ext == ("png") || ext == ("jpg") || ext == ("gif") || ext == ("svg")) {
+                                $(divUploadSelector).append("<div class='col-lg-12  media-body mg-l-15'>"
+                                        +"<img class='wd-40 rounded-circle mg-r-20' src='upload/" + data + "'/>"
+                                        + "<a  href='upload/" + data + "'>دانلود  " + title + "</a>"
+                                        + " <input class='" + databaseColoumnName.replace("#", "") + "' type='hidden'  value='" + data + "'>"
+                                        +"<div class='btn btn-danger btn-icon mg-r-5 mg-b-10' onclick='$(this).parent().remove();'><i class='fa fa-close'></i>"+ "</div>"
+                                        + "</div>");
+                            } else {
+                                $(divUploadSelector).append("<div class='col-lg-12  media-body mg-l-15'>"
+                                        + "<i class='icon ion-ios-paper-outline'></i>"
+                                        + "<a  href='upload/" + data + "'>دانلود  " + title + "</a>"
+                                        + " <input class='" + databaseColoumnName.replace("#", "") + "' type='hidden'  value='" + data + "'>"
+                                        +"<div class='btn btn-danger btn-icon mg-r-5 mg-b-10' onclick='$(this).parent().remove();'><i class='fa fa-close'></i>"+ "</div>"
+                                        + "</div>");
+                            }
+                        } else {
+                            new jj('حجم فایل شما بیش اندازه بزرگ می باشد.').jjModal();
+                        }
+                    } else {
+                        new jj('فایل به درستی ارسال نشد.').jjModal();
+                    }
+                    $("#" + inputFiletitle.replace("#", "")).val('');
+                    $('#user_pic').html('');
+
+                }
+
+            });
+        });
+    };
 
 //    this.jjAjaxFileUploadTitleUploadFiles = function (inputFileId, inputTextSelector, inputFiletitle, inputTilteSelector, divUploadSelector) {
 //        $(this.selector).click(function () {
@@ -2196,11 +2274,10 @@ var jj = function (selector) {
                     url: 'UploadServlet',
                     secureuri: false,
                     fileElementId: inputFileId.replace("#", ""),
-
                     dataType: 'JSON',
                     cache: false,
                     success: function (data) {
-                        alert(data);
+                        new jj(data).jjModal('فایل بدرسیت بارگذاری شد');
 
                         data = data == null ? "" : data;
 
@@ -2225,17 +2302,16 @@ var jj = function (selector) {
                                 $(btn).show();
                                 $("#" + inputFileId.replace("#", "")).show();
 
-                                alert('.حجم فایل شما بیش اندازه بزرگ می باشد');
+                                new jj('حجم فایل شما بیش اندازه بزرگ می باشد').jjModal('خطا');
                             }
                         } else {
-                            alert('.فایل به درستی ارسال نشد');
+                            new jj('فایل به درستی ارسال نشد').jjModal('خطا');
                             $(inputTextSelector).val('');
                             $(inputTextSelector).hide();
 
                             $(btn).show();
                             $("#" + inputFileId.replace("#", "")).show();
-
-                            alert('.حجم فایل شما بیش اندازه بزرگ می باشد');
+                            new jj('حجم فایل شما بیش اندازه بزرگ می باشد').jjModal('خطا');
                         }
                     }
 
