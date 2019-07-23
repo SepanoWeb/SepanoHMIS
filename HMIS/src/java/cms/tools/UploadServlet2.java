5965f037954bc481eb90327bbe23ea5894e8531a
mrsalesi <mrsalesi@gmail.com>
2019-07-23 13:24:28 +0430

    کپی فایل ها  از فولدر قبلی لوکال به فولدر جدید لوکال

--- HMIS/src/java/cms/tools/UploadServlet2.java
+++ HMIS/src/java/cms/tools/UploadServlet2.java
@@ -13,6 +13,7 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+
 public class UploadServlet2  extends HttpServlet {
 
     private static long maxSize = 90000000;
